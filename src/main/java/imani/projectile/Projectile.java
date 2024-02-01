b233034951899009b6aa3370489d3693d5604617
AImani2 <121688634+AImani2@users.noreply.github.com>
2024-01-31 19:46:27 -0500

    projectile homework

---
+++ src/main/java/imani/projectile/Projectile.java
@@ -0,0 +1,35 @@
+package imani.projectile;
+
+public class Projectile {
+
+    private static final double GRAVITY = 9.8;
+    private static final double HALF = 0.5;
+    private double angle;
+    private final double velocity;
+    private double seconds;
+    private final double radians;
+
+    public Projectile(double angle, double velocity) {
+        this.angle = angle;
+        this.velocity = velocity;
+        this.radians = Math.toRadians(angle);
+    }
+
+    public void setSeconds(double seconds) {
+        this.seconds = seconds;
+    }
+
+
+    public double getX() {
+        return Math.cos(radians) * velocity * seconds;
+    }
+
+    public double getY() {
+        return Math.sin(radians) * velocity * seconds - (HALF * GRAVITY * seconds * seconds);
+    }
+
+    public double getApexTime() {
+        return (velocity * Math.sin(radians)) / GRAVITY;
+    }
+
+}
