229d1f53f0fa79ae72beac12ff9563bc84608926
AImani2 <121688634+AImani2@users.noreply.github.com>
2024-02-21 18:44:44 -0500

    correctionsagain

--- src/main/java/imani/projectile/ProjectileFrame.java
+++ src/main/java/imani/projectile/ProjectileFrame.java
@@ -7,6 +7,7 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ProjectileFrame extends JFrame {
     private JTextField velocityField;
@@ -25,89 +26,52 @@ public class ProjectileFrame extends JFrame {
         setLayout(new GridLayout(8, 2));
 
         JLabel velocityLabel = new JLabel("Velocity");
-        JLabel angleLabel = new JLabel("Angle");
-        JLabel secondsLabel = new JLabel("Seconds");
-        JLabel xLabel = new JLabel("X");
-        JLabel yLabel = new JLabel("Y");
-        JLabel peakYLabel = new JLabel("Peak Y");
-        JLabel interceptXLabel = new JLabel("Intercept X");
-        JLabel nothingLabel = new JLabel(" ");
-
         velocityField = new JTextField();
+        add(velocityLabel);
+        add(velocityField);
 
+        JLabel angleLabel = new JLabel("Angle");
         angleSlider = new JSlider(JSlider.HORIZONTAL, 0, 90, 0);
+        add(angleLabel);
+        add(angleSlider);
+
         angleSlider.setMajorTickSpacing(10);
         angleSlider.setMinorTickSpacing(1);
         angleSlider.setPaintTicks(true);
         angleSlider.setPaintLabels(true);
 
+        JLabel secondsLabel = new JLabel("Seconds");
         secondsField = new JTextField();
-        xField = new JLabel();
-        yField = new JLabel();
-        peakYField = new JLabel();
-        interceptXField = new JLabel();
-
-        //add a row that displays peak y and x intercept and further instructions to changing the value of velocity
-        //the fields automatically get updated without pressing calculate
-
-        JButton calculate = new JButton("Calculate");
-
-        add(velocityLabel);
-        add(velocityField);
-
-        add(angleLabel);
-        add(angleSlider);
-
         add(secondsLabel);
         add(secondsField);
 
+        JLabel xLabel = new JLabel("X");
+        xField = new JLabel();
         add(xLabel);
         add(xField);
 
+        JLabel yLabel = new JLabel("Y");
+        yField = new JLabel();
         add(yLabel);
         add(yField);
 
+        JLabel peakYLabel = new JLabel("Peak Y");
+        peakYField = new JLabel();
         add(peakYLabel);
         add(peakYField);
 
+        JLabel interceptXLabel = new JLabel("Intercept X");
+        interceptXField = new JLabel();
         add(interceptXLabel);
         add(interceptXField);
 
+        JLabel nothingLabel = new JLabel(" ");
+        JButton calculate = new JButton("Calculate");
         add(nothingLabel);
         add(calculate);
 
-        calculate.addActionListener(new AbstractAction() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                //this is how you get a button to do something
-                Projectile projectile = new Projectile(
-                        Double.parseDouble(velocityField.getText()), Double.parseDouble(String.valueOf(angleSlider.getValue()))
-                );
-                projectile.setSeconds(Double.parseDouble(secondsField.getText()));
-                xField.setText(Double.toString(projectile.getX()));
-                yField.setText(Double.toString(projectile.getY()));
-                peakYField.setText(Double.toString(projectile.getPeakY()));
-                interceptXField.setText(Double.toString(projectile.getInterceptX()));
-            }
-        });
-
-//        ChangeListener listener = new ChangeListener() {
-//            public void stateChanged(ChangeEvent e) {
-//                JSlider source = (JSlider) e.getSource();
-//                // Check if the user is still adjusting the slider
-//                if (!source.getValueIsAdjusting()) {
-//                    // Get the current value of the slider
-//                    int value = source.getValue();
-//                    // Print the value to the console
-//                    System.out.println(value);
-//                }
-//            }
-//        };
-
+        calculate.addActionListener(e -> updateProjectile());
         angleSlider.addChangeListener(e -> updateProjectile());
-
         velocityField.getDocument().addDocumentListener((SimpleDocumentListener) e -> updateProjectile());
         secondsField.getDocument().addDocumentListener((SimpleDocumentListener) e -> updateProjectile());
     }
