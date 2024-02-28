2a3a5a2391e10491f28a276a741abbe6b96d9311
AImani2 <121688634+AImani2@users.noreply.github.com>
2024-02-28 12:41:57 -0500

    tryinggg

--- src/main/java/imani/projectile/ProjectileGraph.java
+++ src/main/java/imani/projectile/ProjectileGraph.java
@@ -5,33 +5,33 @@ import java.awt.*;
 
 public class ProjectileGraph extends JComponent {
 
-    Projectile projectile = new Projectile(0,0);
+    Projectile projectile = new Projectile(35,65);
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        int centerX = 150;
-        int centerY = 150;
-        int radius = 100;
-        int startAngle = 45;
-        int arcAngle = (int) projectile.getAngle();
+        // divide that time up in the for loop and call the projectile
+        // function at each of those slots and get x and y there.
+        // at those points draw an oval
+        // at every other slot draw a line from start x and start y to end x and end y
 
-        int startX = (int) projectile.getX();
-        int startY = (int) projectile.getY();
+        // im gonna try for 8 slots
+        //getApexTime * 2
+        // the projectile has to have all the same info except for the time
+        double totalTime = (projectile.getApexTime() * 2);
+        double timeSlot = totalTime / 8;
+        // increment by 1/8 each time
+        for (int i = 0; i <= totalTime; i+= .125) //does this increment properly?
+        {
+            int startX = (int) projectile.getX();
+            int startY = (int) projectile.getY();
+            projectile.setSeconds(i + .125);
+            g.drawLine(startX, - startY, (int) projectile.getX(), (int) - projectile.getY());
 
-        int endX = (int) (centerX + radius * Math.cos(Math.toRadians(startAngle + arcAngle)));
-        int endY = (int) (centerY - radius * Math.sin(Math.toRadians(startAngle + arcAngle)));
-
-        g.setColor(Color.black);
-        for (int i = startAngle; i < startAngle + arcAngle; i++) {
-            int nextX = (int) (centerX + radius * Math.cos(Math.toRadians(i + 1)));
-            int nextY = (int) (centerY - radius * Math.sin(Math.toRadians(i + 1)));
-            g.drawLine(startX, - startY, nextX, - nextY);
-            startX = nextX;
-            startY = nextY;
         }
-        g.fillOval(100,(int) projectile.getY(),10, 10);
+
+        g.fillOval(100,(int) projectile.getPeakY(),10, 10);
     }
 
     public void setProjectile(Projectile projectile){
@@ -41,8 +41,3 @@ public class ProjectileGraph extends JComponent {
     }
 }
 
-// draw a graph
-// the graph will be the path of the projectile - arch - a series of lines - try with a series of lines
-// add one dot to the peak - thats it.
-// PR need a screenshot. every UI work needs a screenshot - attach it to the description field in the PR
-// label
